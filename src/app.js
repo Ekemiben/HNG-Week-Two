da4d8002a03074d9c99fbd016bf5dac242645cb1
Ekemini Sunday Ben <ekemiben.4@gmail.com>
2025-02-09 20:07:19 +0100

    New One

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+app.get('/', (req, res) => {
+    res.send('Welcome to the Number Classification API Use /api/classify-number?number=371 to get started.');
+});
 app.use('/api', numberRoutes);
 
 export default app;
