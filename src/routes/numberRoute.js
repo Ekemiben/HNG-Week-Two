9ba76d619172076cd22ffa16fd1010b46e932286
Ekemini Sunday Ben <ekemiben.4@gmail.com>
2025-02-09 21:28:27 +0100

    last commit

--- src/routes/numberRoute.js
+++ src/routes/numberRoute.js
@@ -1,31 +1,3 @@
-
-// import express from 'express';
-// import { classifyNumber } from '../services/numberService.js';
-
-// const router = express.Router();
-
-// router.get('/classify-number', (req, res) => {
-//     const { number } = req.query;
-
-//     const parsedNumber = parseInt(number);
-//     if (isNaN(parsedNumber)) {
-//         return res.status(400).json({
-//             number,
-//             error: true
-//         });
-//     }
-
-//     const result = classifyNumber(parsedNumber);
-//     return res.status(200).json(result);
-// });
-
-// export default router;
-
-
-
-
-
-
 import express from 'express';
 import classifyNumber from '../services/numberService.js';
 
